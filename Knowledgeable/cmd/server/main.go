fb55cec8189874c243210b809ccc9026c0f80d09
MrMorsing14 <frmo0006@stud.kea.dk>
2026-02-11 14:00:18 +0100

    created minor packages and minor edits in main.go

--- Knowledgeable/cmd/server/main.go
+++ Knowledgeable/cmd/server/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
-	// Create a new server instance
-	http.HandleFunc("/", func(writer http.ResponseWriter, response *http.Request) {
-		// Handle the request and respond with knowledge data
-		fmt.Println(writer, "Hello Http!")
-		writer.Write([]byte("Welcome to the Knowledgeable server!"))
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, map[string]string{
+			"Title": "Knowledgeable",
+		})
 	})
 
-	// Start the server on port 8080
 	http.ListenAndServe(":8080", nil)
 }
