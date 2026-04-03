26e201b23777f829ab1c5a83d380d60f4b81829c
ras <rabi0002@stud.ek.dk>
2026-04-03 22:50:03 +0200

    test(e2e): updated selectors and redirectors in login and search tests

--- knowledgeable/tests/search.spec.js
+++ knowledgeable/tests/search.spec.js
@@ -1,14 +1,17 @@
 import { test, expect } from '@playwright/test';
 
+test('user can use search function', async ({ page }) => {
+  await page.goto('/');
 
-test('logged in user can use search function', async ({ page }) => {
-    await page.goto('/');
 
-    await page.getByRole('button', { name: 'Go to Search Engine' }).click();
+  await page.getByRole('link', { name: 'Go to Search Engine' }).click();
 
-    await page.getByRole('textbox', { name: 'search' }).fill('g');
 
-    await page.getByRole('button', { name: 'Search' }).click();
+  await expect(page).toHaveURL('/search');
 
-    await expect(page.locator('body')).toContainText('Go Basics');
+  await page.getByPlaceholder('Search').fill('g');
+
+  await page.getByRole('button', { name: 'Search' }).click();
+
+  await expect(page.getByText('Go Basics')).toBeVisible();
 });
