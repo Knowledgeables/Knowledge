cf0b0c789c5013d986f06613b7c2611dfa632299
ras <rabi0002@stud.ek.dk>
2026-04-20 00:09:17 +0200

    fix(playwright): removed unnecessary request import that is alrady fixtured in playweright conf

--- knowledgeable/e2e/full/app.full.spec.js
+++ knowledgeable/e2e/full/app.full.spec.js
@@ -1,15 +1,16 @@
-// e2e/login.spec.js
-import { test, expect, request } from '@playwright/test';
+// e2e/full/app.full.spec.js
+import { test, expect } from '@playwright/test';
 
-test('user can log in with valid credentials', async ({ page, baseURL }) => {
+test('user can log in with valid credentials', async ({ page, request, baseURL }) => {
   const username = `testuser-${Date.now()}`;
   const password = 'testpassword123';
 
-  const api = await request.newContext();
-  await api.post(`${baseURL}/api/register`, {
+  const response = await request.post(`${baseURL}/api/register`, {
     headers: { 'Content-Type': 'application/json' },
     data: { username, email: `${username}@test.com`, password }
   });
+
+  expect(response.ok()).toBeTruthy();
 
   await page.goto('/login');
   await page.getByLabel('Username').fill(username);
